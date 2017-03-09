e852e2b2693a513e9e9a9fc20434c96aff2832a0
osmandin <osmandin@gmail.com>
2017-03-09 15:56:11 -0500

    Update

--- src/opt/test/AbaloneTest.java
+++ src/opt/test/AbaloneTest.java
@@ -6,6 +6,8 @@ import opt.example.*;
 import opt.ga.*;
 import shared.*;
 import func.nn.backprop.*;
+import shared.reader.CSVDataSetReader;
+import shared.reader.DataSetReader;
 
 import java.util.*;
 import java.io.*;
@@ -20,9 +22,14 @@ import java.text.*;
  * @version 1.0
  */
 public class AbaloneTest {
-    private static Instance[] instances = initializeInstances();
+   private static Instance[] instances = initializeInstances();
+   private static Instance[] instances_test = initializeTestInstances();
 
-    private static int inputLayer = 7, hiddenLayer = 5, outputLayer = 1, trainingIterations = 1000;
+    //private static Instance[] instances = initializeInstances("digitsTrain.csv", "digitsTrainLabels.csv");
+   // private static Instance[] instances_test = initializeInstances("digitsTest.csv", "digitsTestLabels.csv");
+
+
+    private static int inputLayer = 64, hiddenLayer = 10, outputLayer = 1, trainingIterations = 100;
     private static BackPropagationNetworkFactory factory = new BackPropagationNetworkFactory();
     
     private static ErrorMeasure measure = new SumOfSquaresError();
@@ -33,24 +40,40 @@ public class AbaloneTest {
     private static NeuralNetworkOptimizationProblem[] nnop = new NeuralNetworkOptimizationProblem[3];
 
     private static OptimizationAlgorithm[] oa = new OptimizationAlgorithm[3];
+
+    // the following optimization algorithms will be applied:
+
     private static String[] oaNames = {"RHC", "SA", "GA"};
+
+    // String to hold the data in
     private static String results = "";
+    private static String testResults = "";
 
     private static DecimalFormat df = new DecimalFormat("0.000");
 
     public static void main(String[] args) {
+
+        // Create networks
+
         for(int i = 0; i < oa.length; i++) {
             networks[i] = factory.createClassificationNetwork(
                 new int[] {inputLayer, hiddenLayer, outputLayer});
             nnop[i] = new NeuralNetworkOptimizationProblem(set, networks[i], measure);
         }
 
+        // Init algorithms:
+
         oa[0] = new RandomizedHillClimbing(nnop[0]);
         oa[1] = new SimulatedAnnealing(1E11, .95, nnop[1]);
         oa[2] = new StandardGeneticAlgorithm(200, 100, 10, nnop[2]);
 
+        // For each algorithm
+
         for(int i = 0; i < oa.length; i++) {
             double start = System.nanoTime(), end, trainingTime, testingTime, correct = 0, incorrect = 0;
+
+            // train it
+
             train(oa[i], networks[i], oaNames[i]); //trainer.train();
             end = System.nanoTime();
             trainingTime = end - start;
@@ -61,6 +84,9 @@ public class AbaloneTest {
 
             double predicted, actual;
             start = System.nanoTime();
+
+            // apply on training data:
+
             for(int j = 0; j < instances.length; j++) {
                 networks[i].setInputValues(instances[j].getData());
                 networks[i].run();
@@ -79,9 +105,37 @@ public class AbaloneTest {
                         "\nIncorrectly classified " + incorrect + " instances.\nPercent correctly classified: "
                         + df.format(correct/(correct+incorrect)*100) + "%\nTraining time: " + df.format(trainingTime)
                         + " seconds\nTesting time: " + df.format(testingTime) + " seconds\n";
+
+
+            // apply on test data:
+
+
+            for(int j = 0; j < instances_test.length; j++) {
+                networks[i].setInputValues(instances_test[j].getData());
+                networks[i].run();
+
+                predicted = Double.parseDouble(instances_test[j].getLabel().toString());
+                actual = Double.parseDouble(networks[i].getOutputValues().toString());
+
+                double trash = Math.abs(predicted - actual) < 0.5 ? correct++ : incorrect++;
+
+            }
+            end = System.nanoTime();
+            testingTime = end - start;
+            testingTime /= Math.pow(10,9);
+
+            testResults += "\nResults for " + oaNames[i] + ": \nCorrectly classified " + correct + " instances." +
+                    "\nIncorrectly classified " + incorrect + " instances.\nPercent correctly classified: "
+                    + df.format(correct / (correct + incorrect) * 100);
+
         }
 
         System.out.println(results);
+
+        System.out.println("Test results:");
+
+        System.out.println(testResults);
+
     }
 
     private static void train(OptimizationAlgorithm oa, BackPropagationNetwork network, String oaName) {
@@ -106,20 +160,20 @@ public class AbaloneTest {
 
     private static Instance[] initializeInstances() {
 
-        double[][][] attributes = new double[4177][][];
+        double[][][] attributes = new double[3823][][];
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(new File("src/opt/test/abalone.txt")));
+            BufferedReader br = new BufferedReader(new FileReader(new File("src/opt/test/optdigits.txt")));
 
             for(int i = 0; i < attributes.length; i++) {
                 Scanner scan = new Scanner(br.readLine());
                 scan.useDelimiter(",");
 
                 attributes[i] = new double[2][];
-                attributes[i][0] = new double[7]; // 7 attributes
+                attributes[i][0] = new double[64]; // 7 attributes
                 attributes[i][1] = new double[1];
 
-                for(int j = 0; j < 7; j++)
+                for(int j = 0; j < 64; j++)
                     attributes[i][0][j] = Double.parseDouble(scan.next());
 
                 attributes[i][1][0] = Double.parseDouble(scan.next());
@@ -134,9 +188,77 @@ public class AbaloneTest {
         for(int i = 0; i < instances.length; i++) {
             instances[i] = new Instance(attributes[i][0]);
             // classifications range from 0 to 30; split into 0 - 14 and 15 - 30
-            instances[i].setLabel(new Instance(attributes[i][1][0] < 15 ? 0 : 1));
+            //instances[i].setLabel(new Instance(attributes[i][1][0] < 15 ? 0 : 1));
+            instances[i].setLabel(new Instance(attributes[i][1][0]));
+           // System.out.println("Set label:" + instances[i].getLabel());
+
         }
 
         return instances;
     }
+
+
+    private static Instance[] initializeTestInstances() {
+
+        double[][][] attributes = new double[1796][][];
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File("src/opt/test/optsdigits-test.txt")));
+
+            for(int i = 0; i < attributes.length; i++) {
+                Scanner scan = new Scanner(br.readLine());
+                scan.useDelimiter(",");
+
+                attributes[i] = new double[2][];
+                attributes[i][0] = new double[64]; // 7 attributes
+                attributes[i][1] = new double[1];
+
+                for(int j = 0; j < 64; j++)
+                    attributes[i][0][j] = Double.parseDouble(scan.next());
+
+                attributes[i][1][0] = Double.parseDouble(scan.next());
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        Instance[] instances = new Instance[attributes.length];
+
+        for(int i = 0; i < instances.length; i++) {
+            instances[i] = new Instance(attributes[i][0]);
+            // classifications range from 0 to 30; split into 0 - 14 and 15 - 30
+            //instances[i].setLabel(new Instance(attributes[i][1][0] < 15 ? 0 : 1));
+            instances[i].setLabel(new Instance(attributes[i][1][0]));
+
+        }
+
+        return instances;
+    }
+
+    // TODO perhaps use this because of the 7 thing above
+
+    private static Instance[] initializeInstances(String dataFile, String labelFile) {
+        DataSetReader dsr = new CSVDataSetReader(new File("").getAbsolutePath() + "/data/" + dataFile);
+        DataSetReader lsr = new CSVDataSetReader(new File("").getAbsolutePath() + "/data/" + labelFile);
+        DataSet ds;
+        DataSet labs;
+
+        try {
+            ds = dsr.read();
+            labs = lsr.read();
+            Instance[] instances = ds.getInstances();
+            Instance[] labels = labs.getInstances();
+
+            for(int i = 0; i < instances.length; i++) {
+                instances[i].setLabel(new Instance(labels[i].getData()));
+            }
+
+            return instances;
+        } catch (Exception e) {
+            System.out.println("Failed to read input file");
+            return null;
+        }
+    }
+
 }
